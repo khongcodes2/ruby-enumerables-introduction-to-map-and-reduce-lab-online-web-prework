2fe0bfe9e9fced2dc4b3629b01ab11052b4cf1ff
K Hong <albtrossfacefall@gmail.com>
2019-08-12 18:19:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,7 +56,7 @@ def reduce_to_all_true(source_array)
   i=0
   while i<source_array.length do
     #if false abort, if true keep testing
-    if source_array[i]=false
+    if source_array[i]==false
       i=source_array.length
       flag=false
     end
