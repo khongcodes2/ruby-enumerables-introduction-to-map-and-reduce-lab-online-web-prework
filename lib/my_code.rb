807c39bd0c73d48da4ef60c818a36d19929568ac
K Hong <albtrossfacefall@gmail.com>
2019-08-12 17:49:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+def map_to_negativize(source_array)
+  #initialize counter and array
+  i=0
+  negativized_array=[]
+  while i<=source_array.length do
+    negativized_array[i]=source_array[i]*(-1)
+    i+=1
+  end
+  negativized_array
+end
