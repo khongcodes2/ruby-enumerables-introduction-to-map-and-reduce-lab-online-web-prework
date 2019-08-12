c601118ce7bf2555d4c9c00de74385223bda8980
K Hong <albtrossfacefall@gmail.com>
2019-08-12 17:56:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,5 +23,19 @@ end
 def map_to_double(source_array)
   i=0
   doubled_array=[]
-  while i<source_array.length
+  while i<source_array.length do
+    doubled_array[i]=source_array[i]
+    i+=1
+  end
+  doubled_array
+end
+
+def map_to_square(source_array)
+  i=0
+  squared_array=[]  
+  while i<source_array.length do
+    squared_array[i]=source_array[i]**2
+    i+=1
+  end
+  squared_array
 end
